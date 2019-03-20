6ff0e01dc3b7ce8735ec5e09dad8206172122ec0
Kyle Hickinson <git@kylehickinson.com>
2019-03-20 11:23:52 -0400

    Add ads coming soon graphic

--- BraveRewardsUI/Settings/Ads/SettingsAdSectionView.swift
+++ BraveRewardsUI/Settings/Ads/SettingsAdSectionView.swift
@@ -8,6 +8,7 @@ public class SettingsAdSectionView: SettingsSectionView {
   
   private struct UX {
     static let titleColor = UIColor(hex: 0xB13677) // No close color in Brave palette
+    static let comingSoonTextColor = UIColor(hex: 0xC9B5DE) // Has to match icon color (which has no close color)
   }
   
   public override init(frame: CGRect) {
@@ -16,6 +17,7 @@ public class SettingsAdSectionView: SettingsSectionView {
     addSubview(stackView)
     stackView.addArrangedSubview(titleLabel)
     stackView.addArrangedSubview(bodyLabel)
+    stackView.addArrangedSubview(comingSoonView)
     
     stackView.snp.makeConstraints {
       $0.edges.equalTo(self.layoutMarginsGuide)
@@ -38,5 +40,50 @@ public class SettingsAdSectionView: SettingsSectionView {
     $0.textColor = SettingsUX.bodyTextColor
     $0.numberOfLines = 0
     $0.font = SettingsUX.bodyFont
+  }
+  
+  private let comingSoonView = ComingSoonView()
+}
+
+extension SettingsAdSectionView {
+  private class ComingSoonView: UIView {
+    
+    override init(frame: CGRect) {
+      super.init(frame: frame)
+      
+      addSubview(comingSoonStackView)
+      comingSoonStackView.addArrangedSubview(comingSoonImageView)
+      comingSoonStackView.addArrangedSubview(comingSoonLabel)
+      
+      comingSoonStackView.snp.makeConstraints {
+        $0.centerX.equalTo(self)
+        $0.leading.greaterThanOrEqualTo(self)
+        $0.trailing.lessThanOrEqualTo(self)
+        $0.top.bottom.equalTo(self).inset(10.0)
+      }
+    }
+    
+    @available(*, unavailable)
+    required init(coder: NSCoder) {
+      fatalError()
+    }
+    
+    private let comingSoonStackView = UIStackView().then {
+      $0.alignment = .center
+      $0.spacing = 20.0
+    }
+    
+    private let comingSoonImageView = UIImageView(image: UIImage(frameworkResourceNamed: "ads-graphic")).then {
+      $0.setContentHuggingPriority(UILayoutPriority(rawValue: 999.0), for: .horizontal)
+      $0.setContentCompressionResistancePriority(.required, for: .horizontal)
+    }
+    
+    private let comingSoonLabel = UILabel().then {
+      $0.textColor = UX.comingSoonTextColor
+      $0.font = .systemFont(ofSize: 17.0, weight: .medium)
+      $0.numberOfLines = 0
+      $0.setContentCompressionResistancePriority(.required, for: .horizontal)
+      $0.text = BATLocalizedString("BraveRewardsSettingsAdsComingSoonText", "Coming soon.")
+    }
   }
 }
