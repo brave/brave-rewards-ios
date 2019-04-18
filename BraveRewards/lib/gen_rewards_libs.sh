85392d360b4c3077cebb93e680f1bc62d007c9a5
Kyle Hickinson <git@kylehickinson.com>
2019-04-18 18:49:00 -0400

    Add `--clean`/`--help/-h` arguments to library gen script

--- BraveRewards/lib/gen_rewards_libs.sh
+++ BraveRewards/lib/gen_rewards_libs.sh
@@ -6,19 +6,43 @@ current_dir="`pwd`/`dirname $0`"
 ledger_drop_point="$current_dir/bat-native-ledger"
 ads_drop_point="$current_dir/bat-native-ads"
 
-skip_update=`[[ "$1" = "--skip-update" ]] && echo true || echo false`
+skip_update=0
+clean=0
 brave_browser_dir="${@: -1}"
+
+function usage() {
+  echo "Usage: ./gen_rewards_libs.sh [--skip-update] [--clean] {\$home/brave/brave-browser}"
+  echo " --skip-update:   Does not pull down any brave-core/brave-browser changes"
+  echo " --clean:         Cleans build directories and runs a gn gen before building"
+  exit 1
+}
+
+for i in "$@"
+do
+case $i in
+    -h|--help)
+    usage
+    ;;
+    --skip-update)
+    skip_update=1
+    shift
+    ;;
+    --clean)
+    clean=1
+    shift
+    ;;
+esac
+done
 
 if [ ! -d "$brave_browser_dir/src/brave" ]; then
   echo "Did not pass in a directory pointing to brave-browser which has already been init"
   echo "(by running \`npm run init\` at its root)"
-  echo "Example usage: ./gen_rewards_libs.sh [--skip-update] {\$home/brave/brave-browser}"
-  exit 1
+  usage
 fi
 
 pushd $brave_browser_dir > /dev/null
 
-if [ "$skip_update" = false ]; then
+if [ "$skip_update" = 0 ]; then
   # Make sure we rebase master to head
   git checkout -- "*" && git pull
   # Do the rest of the work in the src folder
@@ -36,66 +60,65 @@ git reset --hard HEAD
 # TODO: Check if there are any changes made to any of the dependent vendors via git. If no files are changed,
 #       we can just skip building altogether
 
-# TODO: Have option of "clean build" vs regular build. Clean builds run the gn clean/gen
-#       whereas regualar builds simply run skip to the actual ninja build
+if [ "$clean" = 1 ]; then
+  # If this script has already been run, we'll clean out the build folders
+  [[ -d out/sim-release ]] && gn clean out/sim-release
+  [[ -d out/device-release ]] && gn clean out/device-release
 
-# If this script has already been run, we'll clean out the build folders
-[[ -d out/sim-release ]] && gn clean out/sim-release
-[[ -d out/device-release ]] && gn clean out/device-release
+  # Generate ninja files for x86_64 and arm64 builds
+  #
+  # `root_extra_deps` may have to be extended if more deps from the `//brave` directory are added due to
+  # brave-browser#3080 mentioned above
+  gn gen out/sim-release \
+    --args=" \
+      is_debug = false \
+      enable_ios_bitcode = true \
+      use_xcode_clang = true \
+      ios_deployment_target = \"12.0\" \
+      treat_warnings_as_errors = false \
+      enable_nacl = false \
+      enable_stripping = true \
+      is_official_build = true \
+      symbol_level = 2 \
+      is_chrome_branded = false \
+      target_os=\"ios\" \
+      target_cpu=\"x64\" \
+      root_extra_deps = [ \
+        \"//brave/vendor/bat-native-anonize:anonize2\", \
+        \"//brave/vendor/bat-native-ledger:bat-native-ledger-standalone\", \
+        \"//brave/vendor/bat-native-rapidjson\", \
+        \"//brave/vendor/bat-native-tweetnacl:tweetnacl\", \
+        \"//brave/vendor/bip39wally-core-native:bip39wally-core\", \
+        \"//brave/vendor/challenge_bypass_ristretto_ffi\", \
+        \"//brave/vendor/bat-native-ads:bat-native-ads-standalone\", \
+        \"//brave/vendor/bat-native-confirmations:challenge_bypass_libs\" \
+      ]"
 
-# Generate ninja files for x86_64 and arm64 builds
-#
-# `root_extra_deps` may have to be extended if more deps from the `//brave` directory are added due to
-# brave-browser#3080 mentioned above
-gn gen out/sim-release \
-  --args=" \
-    is_debug = false \
-    enable_ios_bitcode = true \
-    use_xcode_clang = true \
-    ios_deployment_target = \"12.0\" \
-    treat_warnings_as_errors = false \
-    enable_nacl = false \
-    enable_stripping = true \
-    is_official_build = true \
-    symbol_level = 2 \
-    is_chrome_branded = false \
-    target_os=\"ios\" \
-    target_cpu=\"x64\" \
-    root_extra_deps = [ \
-      \"//brave/vendor/bat-native-anonize:anonize2\", \
-      \"//brave/vendor/bat-native-ledger:bat-native-ledger-standalone\", \
-      \"//brave/vendor/bat-native-rapidjson\", \
-      \"//brave/vendor/bat-native-tweetnacl:tweetnacl\", \
-      \"//brave/vendor/bip39wally-core-native:bip39wally-core\", \
-      \"//brave/vendor/challenge_bypass_ristretto_ffi\", \
-      \"//brave/vendor/bat-native-ads:bat-native-ads-standalone\", \
-      \"//brave/vendor/bat-native-confirmations:challenge_bypass_libs\" \
-    ]"
-
-gn gen out/device-release \
-  --args=" \
-    is_debug = false \
-    enable_ios_bitcode = true \
-    use_xcode_clang = true \
-    ios_deployment_target = \"12.0\" \
-    treat_warnings_as_errors = false \
-    enable_nacl = false \
-    enable_stripping = true \
-    is_official_build = true \
-    symbol_level = 2 \
-    is_chrome_branded = false \
-    target_os=\"ios\" \
-    target_cpu=\"arm64\" \
-    root_extra_deps = [ \
-      \"//brave/vendor/bat-native-anonize:anonize2\", \
-      \"//brave/vendor/bat-native-ledger:bat-native-ledger-standalone\", \
-      \"//brave/vendor/bat-native-rapidjson\", \
-      \"//brave/vendor/bat-native-tweetnacl:tweetnacl\", \
-      \"//brave/vendor/bip39wally-core-native:bip39wally-core\", \
-      \"//brave/vendor/challenge_bypass_ristretto_ffi\", \
-      \"//brave/vendor/bat-native-ads:bat-native-ads-standalone\", \
-      \"//brave/vendor/bat-native-confirmations:challenge_bypass_libs\" \
-    ]"
+  gn gen out/device-release \
+    --args=" \
+      is_debug = false \
+      enable_ios_bitcode = true \
+      use_xcode_clang = true \
+      ios_deployment_target = \"12.0\" \
+      treat_warnings_as_errors = false \
+      enable_nacl = false \
+      enable_stripping = true \
+      is_official_build = true \
+      symbol_level = 2 \
+      is_chrome_branded = false \
+      target_os=\"ios\" \
+      target_cpu=\"arm64\" \
+      root_extra_deps = [ \
+        \"//brave/vendor/bat-native-anonize:anonize2\", \
+        \"//brave/vendor/bat-native-ledger:bat-native-ledger-standalone\", \
+        \"//brave/vendor/bat-native-rapidjson\", \
+        \"//brave/vendor/bat-native-tweetnacl:tweetnacl\", \
+        \"//brave/vendor/bip39wally-core-native:bip39wally-core\", \
+        \"//brave/vendor/challenge_bypass_ristretto_ffi\", \
+        \"//brave/vendor/bat-native-ads:bat-native-ads-standalone\", \
+        \"//brave/vendor/bat-native-confirmations:challenge_bypass_libs\" \
+      ]"
+fi
 
 # Build both ledger and ads for both sim/device
 ninja -C out/sim-release bat-native-ledger-standalone bat-native-ads-standalone
@@ -105,14 +128,17 @@ ninja -C out/device-release bat-native-ledger-standalone bat-native-ads-standalo
 lipo -create out/sim-release/libbat-native-ledger-standalone.a \
              out/device-release/libbat-native-ledger-standalone.a \
              -output "$ledger_drop_point/libbat-native-ledger.a"
+echo "Created static library: $ledger_drop_point/libbat-native-ledger.a"
 
 lipo -create out/sim-release/libbat-native-ads-standalone.a \
              out/device-release/libbat-native-ads-standalone.a \
              -output "$ads_drop_point/libbat-native-ads.a"
+echo "Created static library: $ads_drop_point/libbat-native-ads.a"
 
 lipo -create out/sim-release/gen/challenge_bypass_ristretto/out/x86_64-apple-ios/release/libchallenge_bypass_ristretto.a \
              out/device-release/gen/challenge_bypass_ristretto/out/aarch64-apple-ios/release/libchallenge_bypass_ristretto.a \
              -output "$ledger_drop_point/libchallenge_bypass_ristretto.a"
+echo "Created static library: $ledger_drop_point/libchallenge_bypass_ristretto.a"
 
 # Copy include headers over. If these libraries ever begin to include Chromium dependencies–such as `base`–
 # in public headers we will also need to copy over those headers
